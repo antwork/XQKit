e82763a875c77b66ef2901cfda89fa7fa9af5d8b
qiang.xu <xuqiang@tentcoo.com>
2016-05-27 21:25:24 +0800

    Initial Commit

---
+++ XQKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XQKit
+//
+//  Created by Bill on 16/5/27.
+//  Copyright © 2016年 XQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XQKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XQKit
+//
+//  Created by Bill on 16/5/27.
+//  Copyright © 2016年 XQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
